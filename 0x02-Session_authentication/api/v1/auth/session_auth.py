b02aee869df9665fc51a042b25738d5436017e7f
atom-data <alatatomondi@gmail.com>
2023-11-09 18:09:56 +0000

    update sessions

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,41 +1,45 @@
 #!/usr/bin/env python3
-""" handles all routes for the views for Session authentication """
-from api.v1.views import app_views
-from flask import abort, jsonify, request
+""" Session authentication """
+from api.v1.auth.auth import Auth
+import uuid
 from models.user import User
-from os import getenv
 
 
-@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
-def login() -> str:
-    """ view for route /auth_session/login, method POST """
-    u_email = request.form.get('email')
-    if not u_email:
-        return jsonify({"error": "email missing"}), 400
-    u_password = request.form.get('password')
-    if not u_password:
-        return jsonify({"error": "password missing"}), 400
-    user = User.search({'email': u_email})
-    if not user:
-        return jsonify({"error": "no user found for this email"}), 404
-    for u in user:
-        if u.is_valid_password(u_password):
-            from api.v1.app import auth
-            session_id = auth.create_session(u.id)
-            user_json = jsonify(u.to_json())
-            user_json.set_cookie(getenv('SESSION_NAME'), session_id)
-            return user_json
-        else:
-            return jsonify({"error": "wrong password"}), 401
+class SessionAuth(Auth):
+    """ Session Auth class"""
+    user_id_by_session_id = {}
 
+    def create_session(self, user_id: str = None) -> str:
+        """ creates a Session ID for a user_id """
+        if user_id is None or not isinstance(user_id, str):
+            return None
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
 
-@app_views.route('/auth_session/logout', methods=['DELETE'],
-                 strict_slashes=False)
-def logout() -> str:
-    """ view for route /auth_session/logout, method DELETE """
-    from api.v1.app import auth
-    destroy_session = auth.destroy_session(request)
-    if destroy_session is False:
-        abort(404)
-    else:
-        return jsonify({}), 200
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """ returns a User ID based on a Session ID """
+        if session_id is None or not isinstance(session_id, str):
+            return None
+        return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """ returns a User instance based on a cookie value """
+        session_cookie = self.session_cookie(request)
+        if session_cookie is None:
+            return None
+        _id = self.user_id_for_session_id(session_cookie)
+        return User.get(_id)
+
+    def destroy_session(self, request=None):
+        """ deletes the user session / logout """
+        if request is None:
+            return False
+        session_id = self.session_cookie(request)
+        if not session_id:
+            return False
+        user_id = self.user_id_for_session_id(session_id)
+        if not user_id:
+            return False
+        del self.user_id_by_session_id[session_id]
+        return True
